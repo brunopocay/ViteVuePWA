9b4b67ee3cd64d8f6229627a9b94585e4d40b42d
Bruno Pocay <brunopocay@gmail.com>
2024-06-18 17:03:10 -0300

    correção para o pwa funcionar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {}
+})
 
 const app = createApp(App)
 
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,38 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'PokeVite',
+        short_name: 'PokeVite',
+        description: 'My PWA using Vite and Vue',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/ViteVuePWA/'
+  base: '/ViteVuePWA/' // Base URL necessária para GitHub Pages
 })
