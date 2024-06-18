7b3f1c4ef634fe51c6dbff9a24750218b631c016
Bruno Pocay <brunopocay@gmail.com>
2024-06-18 17:16:28 -0300

    teste 9992

--- vite.config.js
+++ vite.config.js
@@ -5,37 +5,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'PokeVite',
-        short_name: 'PokeVite',
-        description: 'My PWA using Vite and Vue',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    vue(),VitePWA({registerType: 'autoUpdate'})
   ],
-  // resolve: {
-  //   alias: {
-  //     '@': fileURLToPath(new URL('./src', import.meta.url))
-  //   }
-  // },
-  base: '/ViteVuePWA/', // Adicione esta linha
-  build: {
-    outDir: 'dist'
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
 })
