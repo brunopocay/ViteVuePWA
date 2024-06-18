7f547cfc2388e2e0bd4b6c764a73d1a73d2ba608
Bruno Pocay <brunopocay@gmail.com>
2024-06-18 15:46:52 -0300

    testando projeto vite vue wpa

--- src/components/CardPokemonSelected.vue
+++ src/components/CardPokemonSelected.vue
@@ -4,22 +4,9 @@ const pokemon = defineProps(["name", "xp", "height", "img", "loading"])
 </script>
 
 <template>
-    <div 
-    class="card CardPokemonSelected"
-    :class="loading ? '' : 'animate__animated animate__flipInY'"
-    >
-        <img 
-        v-if="pokemon.name"
-        :src="pokemon.img" 
-        class="card-img-top pt-2"
-        :alt="pokemon.name"
-        >
-        <img 
-        v-else
-        src="../assets/egg_pokemon.svg"
-        class="card-img-top pt-2"
-        alt="???"
-        >
+    <div class="card CardPokemonSelected" :class="loading ? '' : 'animate__animated animate__flipInY'">
+        <img v-if="pokemon.name" :src="pokemon.img" class="card-img-top pt-2" :alt="pokemon.name">
+        <img v-else src="../assets/egg_pokemon.svg" class="card-img-top pt-2" alt="???">
 
         <div class="card-body">
             <h5 class="card-title text-center">{{ pokemon.name || '???' }}</h5>
@@ -53,22 +40,24 @@ const pokemon = defineProps(["name", "xp", "height", "img", "loading"])
 
 
 <style scoped>
-.CardPokemonSelected{
+.CardPokemonSelected {
     height: 75vh;
-    background: rgb(72,63,251);
+    background: rgb(72, 63, 251);
     background: radial-gradient(circle, rgba(220, 194, 106, 0.8) 0%, rgba(244, 0, 0, 0.8) 100%);
 }
-.CardPokemonSelected img{
+
+.CardPokemonSelected img {
     height: 250px;
 }
 
 @media (max-width: 768px) {
-    .CardPokemonSelected{
+    .CardPokemonSelected {
         height: 30vh;
         width: 40%;
         margin: 0 auto 10px auto;
     }
-    .CardPokemonSelected img{
+
+    .CardPokemonSelected img {
         height: 100px;
     }
 }
