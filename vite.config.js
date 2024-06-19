b5ab540bed2552f36534b2814971c2205e6f39a1
Bruno Pocay <brunopocay@gmail.com>
2024-06-19 08:39:38 -0300

    corrigind arquivo vite

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,39 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
-    vue(),VitePWA({registerType: 'autoUpdate'})
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "PokeVite",
+        short_name: "PokeVite",
+        start_url: "/ViteVuePWA/",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
-  base: '/ViteVuePWA/'
-})
+  base: "/ViteVuePWA/",
+});
